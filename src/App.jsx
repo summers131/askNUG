8218c73d9a909a6afc690d6ed55bc3d136f59d9d
summers131 <wyanmoe131@gmail.com>
2024-05-16 14:23:55 +0700

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+
+<div>
+
+<div className='text-lg font-semibold '>AskNUG</div>
+<div className='text-5xl'>
+  <p>Hello, Users </p>
+  <p>How can I help you today?</p>
+</div>
+<div></div>
+</div>
+
+
+
+
+
+
+
+    </div>
+  )
+}
